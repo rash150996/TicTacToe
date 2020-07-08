f2ba29b3373c32b02d10e487cd51fddb6973668a
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2020-07-08 21:58:43 +0530

    [rashmi] Added: The tic tac toe board to be diplayed for clarity

--- tictactoe.sh
+++ tictactoe.sh
@@ -13,6 +13,17 @@ reset(){
 
 reset
 
+board(){
+  echo "    0 1 2"
+  echo "0   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+  echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+  echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+}
+
+echo "The board is:"
+board
+
+printf "\n"
 flip=$(( RANDOM%2 ))
 if [ $flip -eq 1 ]
 then
@@ -31,3 +42,4 @@ else
 		choice="O"
 	fi
 fi
+
